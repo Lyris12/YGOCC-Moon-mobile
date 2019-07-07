6f05a7e003ab809cd30c5b2cb33d51bc42f05936
Clara <lyris89@live.com>
2019-07-07 10:56:37 -0400

    Sync repo with ygocc/Moon@ce17685bd79f93df8cc7c84d9876c77cafe479a1

--- expansions/script/c21730416.lua
+++ expansions/script/c21730416.lua
@@ -1,38 +1,94 @@
---A.O. Nexus (Bug Test)
+--A.O. Assembler
 --By Auramram
 local s,id=GetID()
 function s.initial_effect(c)
-	--activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
-	--set turn activate
+	--link procedure
+	aux.AddLinkProcedure(c,nil,2,2,s.lcheck)
+	c:EnableReviveLimit()
+	--search
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
-	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-	e2:SetRange(LOCATION_FZONE)
-	e2:SetTargetRange(LOCATION_SZONE,0)
-	e2:SetCondition(s.condition)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x719))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,id+1000)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
-	--replace tribute
+	--link summon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e3:SetCode(id)
-	e3:SetRange(LOCATION_FZONE)
-	e3:SetTargetRange(1,0)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetOperation(s.regop)
 	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id+2000)
+	e4:SetCondition(s.spcon)
+	e4:SetCost(s.spcost)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
 end
---set turn activate
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0x719) and c:IsType(TYPE_TRAP)
+--link procedure
+function s.lcheck(g,lc)
+	return g:IsExists(Card.IsLinkSetCard,1,nil,0x719)
 end
-function s.condition(e)
-	local tp=e:GetHandler():GetControler()
-	return Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_SZONE,0,nil)<Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+--search
+function s.thfilter(c)
+	return c:IsSetCard(0x719) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 and g:GetFirst():IsLocation(LOCATION_HAND) then
+		Duel.ConfirmCards(1-tp,g)
+		Duel.BreakEffect()
+		Duel.ShuffleHand(tp)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+		Duel.SendtoDeck(g,nil,1,REASON_EFFECT)
+		Duel.ShuffleDeck(tp)
+	end
+end
+--link summon
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+0x86c0000+RESET_PHASE+PHASE_END,0,1)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x719) and c:IsType(TYPE_LINK)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_LINK,tp,false,false)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)~=0
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsReleasable() end
+	Duel.Release(c,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCountFromEx(tp,tp,e:GetHandler())>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCountFromEx(tp)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local mc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp):GetFirst()
+		if not mc then return end
+		local mat=mc:GetMaterial()
+		mc:SetMaterial(nil)
+		Duel.SpecialSummon(mc,SUMMON_TYPE_LINK,tp,tp,false,false,POS_FACEUP)
+		mc:SetMaterial(mat)
+	end
 end
