643e2c8c82b1db0eed61cda94b36a8cb35e9cd08
Clara <lyris89@live.com>
2020-09-02 07:09:59 -0400

    Sync repo with Lyris12/ygocc-Moon@6e041045f35dcb9042826b616cd77b92d99da987

--- expansions/script/init.lua
+++ expansions/script/init.lua
@@ -71,7 +71,7 @@ dofile("expansions/script/proc_perdition.lua") --Perditions
 dofile("expansions/script/proc_impure.lua") --Impures
 dofile("expansions/script/proc_runic.lua") --Runic
 dofile("expansions/script/proc_magick.lua") --Magick
-dofile("expansions/script/tables.lua")  --Special Tables
+dofile("expansions/script/tables.lua") --Special Tables
 
 Card.IsReason=function(c,rs)
 	local cusrs=rs>>32
