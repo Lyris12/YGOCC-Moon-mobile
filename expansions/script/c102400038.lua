27cb9ad4f2d46ddde4dac21a2206dbf576784bec
Clara <lyris89@live.com>
2020-09-19 14:46:33 -0400

    Sync repo with Lyris12/ygocc-Moon@5811c199868f4163c668de042b7cf95373925ba3

--- expansions/script/c102400038.lua
+++ expansions/script/c102400038.lua
@@ -10,6 +10,7 @@ function cid.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetHintTiming(TIMING_DAMAGE_STEP)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetCondition(cid.condition2)
 	e2:SetOperation(cid.operation2)
@@ -25,11 +26,29 @@ function cid.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SUMMON_PROC)
-	e1:SetRange(LOCATION_FZONE)
 	e1:SetCondition(cid.ntcon)
 	e1:SetOperation(cid.ntop)
 	e1:SetValue(SUMMON_TYPE_NORMAL)
-	c:RegisterEffect(e3)
+	c:RegisterEffect(e1)
+	e3:SetLabelObject(e1)
+end
+function cid.condition2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetCurrentPhase()~=PHASE_DAMAGE or Duel.IsDamageCalculated() then return false end
+	local a=e:GetHandler()
+	local d=a:GetBattleTarget()
+	return d~=nil and (d:IsLevelAbove(1) or d:IsRankAbove(1) or d:GetDimensionNo()>0 or d:GetFuture()>0)
+		and (a:IsRelateToBattle() or d:IsRelateToBattle())
+end
+function cid.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local a=e:GetHandler()
+	local d=a:GetBattleTarget()
+	if not d:IsRelateToBattle() or d:IsImmuneToEffect(e) or not a:IsRelateToBattle() or not a:IsRelateToEffect(e) then return end
+	local e3=Effect.CreateEffect(a)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+	e3:SetValue(Duel.ReadCard(d,CARDDATA_LEVEL)*100)
+	a:RegisterEffect(e3)
 end
 function cid.cfilter(c,tp)
 	return c:IsFaceup() and c:IsLevelAbove(5) and c:GetSummonPlayer()~=tp
@@ -39,28 +58,22 @@ function cid.evcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function cid.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then
-		c:RegisterFlagEffect(id,0,0,1)
-		local e3=e:GetLabelObject()
-		local res=c:IsSummonable(true,e3) or c:IsMSetable(true,e3)
-		c:ResetFlagEffect(id)
-		return res
-	end
+	local e3=e:GetLabelObject()
+	if chk==0 then return c:IsSummonable(true,e3) or c:IsMSetable(true,e3) end
 	Duel.SetOperationInfo(0,CATEGORY_SUMMON,c,1,0,0)
 end
 function cid.sumop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
-	c:RegisterFlagEffect(id,RESET_CHAIN,0,1)
 	local e3=e:GetLabelObject()
 	local pos=0
-	if c:IsSummonable(true,e3,1) then pos=pos+POS_FACEUP_ATTACK end
-	if c:IsMSetable(true,e3,1) then pos=pos+POS_FACEDOWN_DEFENSE end
+	if c:IsSummonable(true,e3) then pos=pos+POS_FACEUP_ATTACK end
+	if c:IsMSetable(true,e3) then pos=pos+POS_FACEDOWN_DEFENSE end
 	if pos==0 then return end
 	if Duel.SelectPosition(tp,c,pos)==POS_FACEUP_ATTACK then
-		Duel.Summon(tp,c,true,e3,1)
+		Duel.Summon(tp,c,true,e3)
 	else
-		Duel.MSet(tp,c,true,e3,1)
+		Duel.MSet(tp,c,true,e3)
 	end
 end
 function cid.filter(c)
@@ -68,43 +81,23 @@ function cid.filter(c)
 end
 function cid.ntcon(e,c,minc)
 	if c==nil then return true end
-	return minc<=1 and (Duel.CheckTribute(c,0) or Duel.CheckTribute(tp,1,1,Duel.GetMatchingGroup(cid.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil))) and c:GetFlagEffect(id)>0
+	return minc<=1 and (Duel.CheckTribute(c,0) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or Duel.CheckTribute(tp,1,1,Duel.GetMatchingGroup(cid.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)))
 end
-function cid.ntop(e,tp,eg,ep,ev,re,r,rp,c)
-	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		local g=Duel.SelectTribute(tp,c,1,1,Duel.GetMatchingGroup(cid.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil))
+function cid.ntop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local mg=Duel.GetMatchingGroup(cid.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if Duel.CheckTribute(c,1,1,mg) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		local g=Duel.SelectTribute(tp,c,1,1,mg)
 		c:SetMaterial(g)
 		Duel.Release(g,REASON_SUMMON+REASON_MATERIAL)
 		e:SetValue(SUMMON_TYPE_ADVANCE)
-	else e:SetValue(SUMMON_TYPE_NORMAL) end
-end
-function cid.condition2(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetCurrentPhase()~=PHASE_DAMAGE or Duel.IsDamageCalculated() then return false end
-	local a=e:GetHandler()
-	local d=a:GetBattleTarget()
-	return d~=nil and (a:IsRelateToBattle() or d:IsRelateToBattle())
-end
-function cid.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local a=e:GetHandler()
-	local d=a:GetBattleTarget()
-	if not d:IsRelateToBattle() or d:IsImmuneToEffect(e) then return end
-	local atk=d:GetAttack()
-	local e1=Effect.CreateEffect(a)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e1:SetValue(math.ceil(atk/2))
-	d:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
-	e2:SetValue(math.ceil(d:GetDefense()/2))
-	d:RegisterEffect(e2)
-	if a:IsRelateToBattle() and a:IsRelateToEffect(e) then
-		local e3=Effect.CreateEffect(a)
-		e3:SetType(EFFECT_TYPE_SINGLE)
-		e3:SetCode(EFFECT_UPDATE_ATTACK)
-		e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
-		e3:SetValue(math.ceil(atk/2))
-		a:RegisterEffect(e3)
+	elseif Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		e:SetValue(SUMMON_TYPE_NORMAL)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_DISABLE)
+		e1:SetValue(-600)
+		c:RegisterEffect(e1)
 	end
 end
