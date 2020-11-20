434a34a22df25526a4f1d16b6f1990c81a263782
Lyris <lyris89@live.com>
2020-11-20 05:25:05 -0500

    Sync repo with Lyris12/ygocc-Moon@2631dba6d3f8b8aa7888e0c06f97fb58058bc638

--- expansions/script/c102400251.lua
+++ expansions/script/c102400251.lua
@@ -23,13 +23,14 @@ function s.con(e,tp)
 	local a=Duel.GetAttacker()
 	if not a then return false end
 	if a:IsControler(1-tp) then a=Duel.GetAttackTarget() end
-	return a~=e:GetHandler() and a:IsSetCard(0xa6c) and a:IsRelateToBattle()
+	return a and a~=e:GetHandler() and a:IsSetCard(0xa6c) and a:IsRelateToBattle()
 		and Duel.GetCurrentPhase()==PHASE_DAMAGE and not Duel.IsDamageCalculated()
 end
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local a=Duel.GetAttacker()
 	if a:IsControler(1-tp) then a=Duel.GetAttackTarget() end
-	if chk==0 then return a:IsAbleToRemove() end
+	a=a:GetBattleTarget()
+	if chk==0 then return a and a:IsAbleToRemove() end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,a,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
 end
@@ -37,7 +38,8 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local a=Duel.GetAttacker()
 	if a:IsControler(1-tp) then a=Duel.GetAttackTarget() end
-	if not (a and a:IsRelateToBattle()) or Duel.Remove(a,POS_FACEUP,REASON_EFFECT+REASON_TEMPORARY)==0
+	a=a:GetBattleTarget()
+	if not a:IsRelateToBattle() or Duel.Remove(a,POS_FACEUP,REASON_EFFECT+REASON_TEMPORARY)==0
 		or a:IsType(TYPE_TOKEN) then return end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -56,10 +58,10 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
 	e1:SetOperation(function(e,tp,eg,ep,ev)
 		local a=Duel.GetAttacker()
-		if not a then return end
 		if a:IsControler(1-tp) then a=Duel.GetAttackTarget() end
+		if not a then return end
 		if a:IsSetCard(0xa6c) and ep~=tp and Duel.SelectEffectYesNo(tp,c) then
-			Duel.ChangeBattleDamage(tp,ev*2)
+			Duel.ChangeBattleDamage(1-tp,ev*2)
 			e1:Reset()
 		end
 	end)
