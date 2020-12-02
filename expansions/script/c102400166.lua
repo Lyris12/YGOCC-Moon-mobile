e5b55f0cdbb161ef6c3e4aa416979c8d08e1b2dc
Lyris <lyris89@live.com>
2020-12-02 18:42:06 -0500

    Add new custom "Cyberdark" support by Lyris

--- expansions/script/c102400166.lua
+++ expansions/script/c102400166.lua
@@ -1,54 +1,107 @@
---created & coded by Lyris, art by whoalisaa of DeviantArt
---贅沢
-local cid,id=GetID()
-function cid.initial_effect(c)
+--created & coded by Lyris, art from "Degenerate Circuit" & at http://www.elasmo-research.org/education/evolution/evol-image/c_taurus.jpg
+--サイバー・ダーク・ファング
+local s,id=GetID()
+function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_TODECK+CATEGORY_DRAW)
-	e1:SetCondition(function() return Duel.GetCurrentPhase()==PHASE_MAIN1 and not Duel.CheckPhaseActivity() end)
-	e1:SetTarget(cid.target)
-	e1:SetOperation(cid.activate)
+	e1:SetDescription(1068)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(s.eqtg)
+	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_RANGE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e4:SetCode(EFFECT_REMOVE_REDIRECT)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetTarget(s.rmtarget)
+	e4:SetTargetRange(0xff,0)
+	e4:SetValue(LOCATION_GRAVE)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,id)
+	e5:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e5:SetDescription(1109)
+	e5:SetCost(s.cost)
+	e5:SetTarget(s.target)
+	e5:SetOperation(s.operation)
+	c:RegisterEffect(e5)
 end
-function cid.filter(c,tp)
-	return c:GetSequence()<20 and c:IsAbleToRemove(tp,POS_FACEDOWN)
+function s.filter(c)
+	return c:IsLevelBelow(3) and c:IsRace(RACE_DRAGON) and not c:IsForbidden()
 end
-function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>24 and Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_DECK,0,20,nil,tp)
-		or Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,20,nil) end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc) end
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
-function cid.activate(e,tp,eg,ep,ev,re,r,rp)
-	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	local g1,g2=Duel.GetMatchingGroup(cid.filter,p,LOCATION_DECK,0,nil,p),Duel.GetMatchingGroup(Card.IsAbleToDeck,p,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
-	local b1,b2=Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>21 and #g1>19,#g2>19
-	if not b1 and not b2 then return end
-	if b2 and (not b1 or Duel.SelectOption(tp,1192,1105)~=0) then
-		if Duel.SendtoDeck(g2:Select(p,20,20,nil),nil,2,REASON_EFFECT)==0 or not g2:IsExists(Card.IsLocation,20,nil,LOCATION_DECK+LOCATION_EXTRA) then return end
-		Duel.ShuffleDeck(p)
-	else
-		Duel.DisableShuffleCheck()
-		if Duel.Remove(g1,POS_FACEDOWN,REASON_EFFECT)==0 then return end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		local atk=tc:GetTextAttack()
+		if atk<0 then atk=0 end
+		if not Duel.Equip(tp,tc,c,false) then return end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(s.eqlimit)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_EQUIP)
+		e2:SetProperty(EFFECT_FLAG_OWNER_RELATE+EFFECT_FLAG_IGNORE_IMMUNE)
+		e2:SetCode(EFFECT_UPDATE_ATTACK)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e2:SetValue(atk)
+		tc:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_EQUIP)
+		e3:SetCode(EFFECT_DESTROY_SUBSTITUTE)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e3:SetValue(s.repval)
+		tc:RegisterEffect(e3)
 	end
-	Duel.BreakEffect()
-	Duel.ConfirmDecktop(p,5)
-	local g=Duel.GetDecktopGroup(p,5)
-	Duel.Hint(HINT_SELECTMSG,p,HINTMSG_ATOHAND)
-	local sg=g:Select(p,2,2,nil)
-	Duel.DisableShuffleCheck()
-	Duel.SendtoHand(sg:Filter(Card.IsAbleToHand,nil),nil,REASON_EFFECT)
-	Duel.SendtoGrave(sg:Filter(aux.NOT(Card.IsAbleToHand),nil),REASON_RULE)
-	local hg=sg:Filter(Card.IsLocation,nil,LOCATION_HAND)
-	if #hg>0 then
-		Duel.ConfirmCards(1-p,hg)
-		Duel.ShuffleHand(p)
-	end
-	Duel.SortDecktop(p,p,3)
-	for i=1,3 do
-		Duel.MoveSequence(Duel.GetDecktopGroup(p,1):GetFirst(),1)
-	end
+end
+function s.eqlimit(e,c)
+	return e:GetOwner()==c
+end
+function s.repval(e,re,r,rp)
+	return bit.band(r,REASON_BATTLE)~=0
+end
+function s.rmtarget(e,c)
+	return not c:IsLocation(0x80) and not c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSetCard(0x4093)
+end
+function s.cfilter(c)
+	return c:IsSetCard(0x4093) and (c:IsFaceup() or c:GetEquipTarget()) and c:IsType(TYPE_EQUIP) and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	Duel.SendtoGrave(Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_ONFIELD,0,1,1,nil),REASON_COST)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.AND(Card.IsSetCard,Card.IsAbleToHand),tp,LOCATION_DECK,0,1,nil,0x4093) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,aux.AND(Card.IsSetCard,Card.IsAbleToHand),tp,LOCATION_DECK,0,1,1,nil,0x4093)
+	Duel.SendtoHand(g,nil,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,g)
 end
